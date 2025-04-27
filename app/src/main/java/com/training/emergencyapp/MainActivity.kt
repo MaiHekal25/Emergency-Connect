7b3364d4f4c3d6cc2942808ec9f9ec2ed4437ed5
Mai Hekal <maihekal25@gmail.com>
2025-04-27 21:09:47 +0300

    feat: set up data and view binding in MainActivity
    This commit initializes an `ArrayList` named `emergencies` in `MainActivity` to store instances of `EmergencyModel`. It populates this list with three emergency entries: "Police" with the phone number "122", "Fire Truck" with "180", and "Child Rescue" with "16000". Additionally, view binding is enabled to bind the layout to `MainActivity` via the variable `binding`.

--- app/src/main/java/com/training/emergencyapp/MainActivity.kt
+++ app/src/main/java/com/training/emergencyapp/MainActivity.kt
@@ -5,16 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.training.emergencyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var emergencies = ArrayList<EmergencyModel>()
+        emergencies.add(EmergencyModel(R.drawable.ic_police, "Police", "122"))
+        emergencies.add(EmergencyModel(R.drawable.ic_fire, "Fire Truck", "180"))
+        emergencies.add(EmergencyModel(R.drawable.ic_child, "Child Rescue", "16000"))
     }
 }
